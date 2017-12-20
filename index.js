ebe6ea422f42a607873c0391d36d3d6a3cd89bce
Степаненко Сергей Вячеславович <stepanenkosv@alt.altarix.ru>
2017-12-20 16:43:06 +0300

    test fetch

--- index.js
+++ index.js
@@ -1,51 +1,31 @@
-console.log('started')
-// const firebase = require('firebase');
-// firebase.initializeApp(config);
+console.log('server started')
+const fetch = require("node-fetch");
+const firebase = require('firebase');
 
-const requestData = ({ CORS, url, key, ...rest }) => {
-	return new Promise((resolve, reject) => {
-		const xhr = new XMLHttpRequest();
-		const wrappedUrl = `${url}&mode=poll`;
-
-		xhr.open('GET', wrappedUrl, true);
-		xhr.onload = function x() {
-			if (this.status === 200) {
-				let response = {};
-
-				try {
-					response = JSON.parse(this.response).payload[0].data;
-				} catch (e) {
-					console.error('JSON.parse error', e); // eslint-disable-line
-				} finally {
-					resolve({
-						response,
-						// ...rest,
-						// key,
-						// url,
-					});
-				}
-			} else {
-				const error = new Error(this.statusText);
-				error.code = this.status;
-				reject({
-					// ...rest,
-					// key,
-					// url,
-					error
-				});
-			}
-		};
-
-		xhr.send();
-		xhr.onerror = () => reject(new Error('oops'));
-	});
+const config = {
+	apiKey: 'AIzaSyCB1TfuGQegOrHOPcFJFqpxDmMTSElXQVg',
+	authDomain: 'plastic-warriors.firebaseapp.com',
+	databaseURL: 'https://plastic-warriors.firebaseio.com',
+	projectId: 'plastic-warriors',
+	storageBucket: '',
+	messagingSenderId: '143541315246',
 };
 
-const url = 'https://www.izhforum.info/forum/izhevsk/tracker_live_map.php?id=motorola.H6ZG8MKE3ZNPBN&pin1=0ac04678341515627428372bfeff4a7f';
+firebase.initializeApp(config);
 
-setInterval(() => {
-    console.log('tick')
-    requestData({ url }).then(result => {
-        console.log(result)
-    })
-}, 10000)
+const database = firebase.database();
+const kidsTrackData = database.ref('/kidsTrackData');
+
+const url = 'https://www.izhforum.info/forum/izhevsk/tracker_live_map.php?id=motorola.H6ZG8MKE3ZNPBN&pin1=0ac04678341515627428372bfeff4a7f&mode=poll';
+
+setTimeout(() => {
+    fetch(url)
+        .then(response => {
+            response.json().then(json => {
+                kidsTrackData.set(json.payload[0].data);
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}, 120000)
